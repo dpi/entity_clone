dfc03d98e4e1daa1087eb062a05297871a259a1c
TrevorBradley <TrevorBradley@770722.no-reply.drupal.org>
2019-03-06 10:22:13 +0100

    Issue #3035700 by TrevorBradley: Undefined index: is_circular

--- src/EntityClone/Content/ContentEntityCloneBase.php
+++ src/EntityClone/Content/ContentEntityCloneBase.php
@@ -151,7 +151,7 @@ class ContentEntityCloneBase implements EntityHandlerInterface, EntityCloneInter
 
         $referenced_entities[] = $cloned_reference;
       }
-      elseif ($child_properties['is_circular']) {
+      elseif (!empty($child_properties['is_circular'])) {
         $referenced_entities[] = $already_cloned[$referenced_entity->getEntityTypeId()][$referenced_entity->id()];
       }
       else {
